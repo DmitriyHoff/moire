6da9175cae91fa50222c88dd5af525792c47aef2
Dmitriy Goff <goffdmitriy@gmail.com>
2023-03-27 10:47:43 +0200

    fix PageHeader

--- moire/src/components/PageHeader.vue
+++ moire/src/components/PageHeader.vue
@@ -1,21 +1,22 @@
 <script setup>
 import { SHOP_PHONE } from '../config.js';
+import { RouterLink } from 'vue-router';
 </script>
 <template>
   <header class="header container">
     <div class="header__wrapper">
       <span class="header__info">Каталог</span>
 
-      <a class="header__logo" href="#">
+      <RouterLink class="header__logo" :to="{ name: 'main' }">
         <img
           src="/img/svg/logo-moire.svg"
           alt="Логотип интернет магазина Moire"
           width="116"
           height="34"
         />
-      </a>
+      </RouterLink>
 
-      <a class="header__tel" href="tel:8 800 600 90 09">{{ SHOP_PHONE }}</a>
+      <a class="header__tel" :href="`tel:${SHOP_PHONE}`">{{ SHOP_PHONE }}</a>
 
       <a class="header__cart" href="cart.html" aria-label="Корзина с товарами">
         <svg v-svg symbol="icon-cart" size="0 0 19 24"></svg>
