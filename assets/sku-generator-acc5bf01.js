1695f9643be0fcf752a33a1cebb379c231a93ee4
DmitriyHoff <goffdmitriy@gmail.com>
2023-04-08 08:04:29 +0200

    add cart button to items

---
+++ assets/sku-generator-acc5bf01.js
@@ -0,0 +1,1 @@
+function a({id:t,color:r,size:S}){return`${t.toString().padStart(3,"0")}${r.toString().padStart(2,"0")}${S.toString().padStart(2,"0")}`}export{a as t};
--- assets/sku-generator-e3173cad.js
+++
@@ -1,1 +0,0 @@
-class g{static get({id:t,color:a,size:r}){return`${t.toString().padStart(3,"0")}${a.toString().padStart(2,"0")}${r.toString().padStart(2,"0")}`}}export{g as S};
